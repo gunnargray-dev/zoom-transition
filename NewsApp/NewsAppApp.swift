7cefb3c671aab724a5e1316fc7123e31f41a1ba3
gunnargray <hi@gunnargray.com>
2025-06-24 11:44:04 -0500

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Gunnar Gray on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
